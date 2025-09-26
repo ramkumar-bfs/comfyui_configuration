7b48cec34d045777f246e2b5a4c9555d2d92aaa6
ram.kumar <ram.kumar@basilicfly.com>
2025-09-26 11:17:44 +0530

    DEVELOPMENT: updated start_server_py313.bat file

--- start_server_py313.bat
+++ start_server_py313.bat
@@ -1,5 +1,3 @@
-@REM .venv_windows_py313\Scripts\python main.py --listen 0.0.0.0 --port 8010 --enable-cors-header --front-end-version Comfy-Org/ComfyUI_frontend@latest --extra-model-paths-config .\extra_model_paths_server.yaml --disable-smart-memory --force-non-blocking --async-offload 
-
 ..\.venv_windows_py313\Scripts\python "..\comfyui_repo\main.py" ^
   --port 8010 ^
   --listen ^
